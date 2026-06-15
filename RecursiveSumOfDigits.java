2c6ddc57f28fb22d0d24f604e7c66c9f7ee9a0d6
GLBITM-25110CN351 <dipendralama38555@gmail.com>
2026-06-15 17:49:41 +0530

    Digits

---
+++ RecursiveSumOfDigits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursiveSumOfDigits {
+
+    // Recursive function to find sum of digits
+    static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0; // base case
+        }
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int result = sumOfDigits(num);
+
+        System.out.println("Sum of digits = " + result);
+    }
+}
